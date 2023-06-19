d8796fc631915b6b531657362c2d9f94441a29a0
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2023-06-19 10:57:26 +0700

    menambahkan int main

---
+++ iofilewithpath/iofilewithpath.cpp
@@ -0,0 +1,21 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string baris;
+	string Namafile;
+
+	cout << "Masukkan Nama File : ";
+	cin >> Namafile;
+
+	//membuka file dalam mode menulis.
+	ofstream outfile;
+	//menunjuk ke sebuah nama file
+	outfile.open(Namafile, ios::out);
+
+	cout << ">= Menulis file, \'q' untuk keluar" << endl;
+	//jika file ada maka
+	
+}
