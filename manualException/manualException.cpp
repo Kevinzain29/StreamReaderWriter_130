d74a46cff8fd52fc46228b2bebafe9ad3b6f7437
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2023-06-19 11:19:38 +0700

    menanmbahkan catch dan return

--- manualException/manualException.cpp
+++ manualException/manualException.cpp
@@ -7,6 +7,15 @@ int main()
 		cout << "Selamat Belajar di prodi TI UMY" << endl;
 		//throw 0,5; //melemparkan sebuah integer maka
 		cout << "Pernyataan tidak akan di eksekusi" << endl;
-
 	}
+	catch (int a) {
+		//blok ini akan dieksekusi 
+		cout << "Pengecualian akan dieksekusi" << endl;
+	}
+	catch (...) {
+		/*jika selain integer makan block ini akan dieksekusi*/
+		cout << "default Pengecualian di eksekusi" << endl;
+	}
+
+	return 0;
 }
